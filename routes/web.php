603db777daf9d3ee26ad89a5593a16040a746cfc
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-08-05 02:10:18 -0300

    Adiciona view de exemplo com bootstrap

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/dale', function() {
+    return view('dale');
+});
+
 require __DIR__.'/auth.php';
