7351d93af22026aba46d2093b081a17d1f63899b
marialuisarg <marialuisariolinoguimaraes@gmail.com>
2022-09-02 09:53:11 -0300

    implementa form produtos, controller e rotas

--- resources/views/produtos/cadastro.blade.php
+++ resources/views/produtos/cadastro.blade.php
@@ -7,21 +7,32 @@
 
     <div class="py-12">
         <x-table-card>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Preço</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                    </tr>
-                </tbody>
-            </table>
+            <form action="{{route('salvarprodutos')}}" method="POST">
+                @csrf
+                <div>
+                    <x-label for="nome" :value="__('Nome')" />
+                    <x-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required autofocus />
+                </div>
+
+                <div class="mt-3">
+                    <x-label for="sabor" :value="__('Sabor')" />
+                    <x-input id="sabor" class="block mt-1 w-full" type="text" name="sabor" :value="old('sabor')" required autofocus />
+                </div>
+
+                <div class="mt-3">
+                    <x-label for="preco" :value="__('Preço')" />
+                    <x-input id="preco" class="block mt-1 w-full" type="text" name="preco" placeholder="00.00" pattern="[0-9]{2}.[0-9]{2}" :value="old('preco')" required autofocus />
+                </div>
+                
+                <div class="mt-3">
+                    <x-label for="descricao" :value="__('Descrição')" />
+                    <x-input id="descricao" class="block mt-1 w-full" type="text" name="descricao" :value="old('descricao')" required autofocus />
+                </div>
+        
+                <x-button-brown class="mt-4">
+                    {{ __('Salvar') }}
+                </x-button-brown>
+            </form>
         </x-table-card>
     </div>
 
--- routes/auth.php
+++ routes/auth.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -54,4 +55,7 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+
+    Route::post('produtos/salvar', [ProductsController::class, 'store'])
+                ->name('salvarprodutos');
 });
